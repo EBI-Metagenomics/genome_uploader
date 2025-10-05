5766971efe9d5217f0b6baaa2993ce968ad55341
Ekaterina Sakharova <kates@ebi.ac.uk>
2025-10-05 22:51:53 +0100

    additions

--- genomeuploader/taxon_finder.py
+++ genomeuploader/taxon_finder.py
@@ -13,13 +13,80 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
-import genomeuploader.ena as ena
+
+import argparse
+import requests
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class TaxonFinder:
     def __init__(self, start_lineage):
         self.lineage = start_lineage
         self.taxid, self.scientific_name = self.extract_tax_info(self.lineage)
+
+    def query_taxid(self, taxid):
+        """
+        Queries ENA taxonomy API for a scientific name given a taxid.
+        Args:
+            taxid (str or int): NCBI taxonomic ID.
+        Returns:
+            str: Scientific name corresponding to the taxid, or empty string if not found.
+        """
+        url = f"https://www.ebi.ac.uk/ena/taxonomy/rest/tax-id/{taxid}"
+        response = requests.get(url)
+
+        try:
+            # Will raise exception if response status code is non-200
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            logger.error(f"Request failed {url} with error {e}")
+            return False
+        res = response.json()
+        return res.get("scientificName", "")
+
+    def query_scientific_name(self, scientific_name, search_rank=False):
+        """
+        Queries ENA taxonomy API for a scientific name and optionally its rank.
+        If the name exists, it checks if it's submittable and retrieves its taxid.
+        Args:
+            scientific_name (str): Scientific name to query.
+            search_rank (bool): If True, also return taxonomic rank.
+        Returns:
+            tuple or bool: If search_rank is True, returns (submittable, taxid,
+            rank). Otherwise, returns (submittable, taxid).
+        """
+        url = f"https://www.ebi.ac.uk/ena/taxonomy/rest/scientific-name/{scientific_name}"
+        response = requests.get(url)
+
+        try:
+            # Will raise exception if response status code is non-200
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            if search_rank:
+                return False, "", ""
+            else:
+                return False, ""
+
+        try:
+            res = response.json()[0]
+        except IndexError:
+            if search_rank:
+                return False, "", ""
+            else:
+                return False, ""
+
+        submittable = res.get("submittable", "").lower() == "true"
+        taxid = res.get("taxId", "")
+        rank = res.get("rank", "")
+
+        if search_rank:
+            return submittable, taxid, rank
+        else:
+            return submittable, taxid
 
     def extract_tax_info(self, tax_info: str) -> tuple[int, str]:
         """
@@ -36,10 +103,12 @@ class TaxonFinder:
         Returns:
             tuple[int, str]: (taxid, scientific_name)
         """
+        taxid = ''
         lineage = tax_info.split(";")
 
         # if unclassified, block the execution and get the official name for the kingdom
         lineage_first = lineage[0]
+        scientific_name = ''
         if "Unclassified " in lineage_first:
             if "Archaea" in lineage_first:
                 scientific_name = "uncultured archaeon"
@@ -47,7 +116,7 @@ class TaxonFinder:
                 scientific_name = "uncultured bacterium"
             elif "Eukaryota" in lineage_first:
                 scientific_name = "uncultured eukaryote"
-            submittable, taxid, rank = ena.query_scientific_name(scientific_name, search_rank=True)
+            submittable, taxid, rank = self.query_scientific_name(scientific_name, search_rank=True)
             return taxid, scientific_name
 
         kingdoms = ["Archaea", "Bacteria", "Eukaryota"]
@@ -80,12 +149,11 @@ class TaxonFinder:
         # level in the taxonomy tree if the identified name is unsubmittable
         iterator = len(lineage) - 1
         submittable = False
-        rank = ""
         while iterator != -1 and not submittable:
             scientific_name = lineage[iterator].strip()
             if digit_annotation:
                 if "*" not in scientific_name:
-                    scientific_name = ena.query_taxid(scientific_name)
+                    scientific_name = self.query_taxid(scientific_name)
                 else:
                     iterator -= 1
                     continue
@@ -94,7 +162,7 @@ class TaxonFinder:
                 scientific_name = scientific_name.split("__")[1]
             else:
                 raise ValueError("Unrecognised taxonomy format: " + scientific_name)
-            submittable, taxid, rank = ena.query_scientific_name(scientific_name, search_rank=True)
+            submittable, taxid, rank = self.query_scientific_name(scientific_name, search_rank=True)
 
             if not submittable:
                 if final_kingdom == "Archaea" or final_kingdom == "2157":
@@ -106,7 +174,6 @@ class TaxonFinder:
             iterator -= 1
 
         return taxid, scientific_name
-
 
     def extract_eukaryota_info(self, name: str, rank: str) -> tuple[bool, str, int]:
         """
@@ -129,26 +196,25 @@ class TaxonFinder:
 
         if rank == "super kingdom":
             name = "uncultured eukaryote"
-            submittable, taxid = ena.query_scientific_name(name)
+            submittable, taxid = self.query_scientific_name(name)
             return submittable, name, taxid
         else:
             name = name.capitalize() + " sp."
-            submittable, taxid = ena.query_scientific_name(name)
+            submittable, taxid = self.query_scientific_name(name)
             if submittable:
                 return submittable, name, taxid
             else:
                 name = "uncultured " + name
-                submittable, taxid = ena.query_scientific_name(name)
+                submittable, taxid = self.query_scientific_name(name)
                 if submittable:
                     return submittable, name, taxid
                 else:
                     name = name.replace(" sp.", "")
-                    submittable, taxid = ena.query_scientific_name(name)
+                    submittable, taxid = self.query_scientific_name(name)
                     if submittable:
                         return submittable, name, taxid
                     else:
                         return non_submittable
-
 
     def extract_bacteria_info(self, name: str, rank: str) -> tuple[bool, str, int]:
         """
@@ -172,7 +238,7 @@ class TaxonFinder:
         elif rank == "genus":
             name = f"{name} sp."
 
-        submittable, taxid, rank = ena.query_scientific_name(name, search_rank=True)
+        submittable, taxid, rank = self.query_scientific_name(name, search_rank=True)
         if not submittable:
             name = name.lower()
             if rank in ["species", "genus", "family"]:
@@ -188,10 +254,9 @@ class TaxonFinder:
                 name = "uncultured gammaproteobacteria bacterium"
             elif "deltaproteobacteria" in name:
                 name = "uncultured delta proteobacterium"
-            submittable, taxid = ena.query_scientific_name(name)
+            submittable, taxid = self.query_scientific_name(name)
 
         return submittable, name, taxid
-
 
     def extract_archaea_info(self, name: str, rank: str) -> tuple[bool, str, int]:
         """
@@ -222,13 +287,36 @@ class TaxonFinder:
         elif rank == "genus":
             name = f"uncultured {name} sp."
 
-        submittable, taxid, rank = ena.query_scientific_name(name, search_rank=True)
+        submittable, taxid, rank = self.query_scientific_name(name, search_rank=True)
         if not submittable:
             if "Candidatus" in name:
                 if rank == "phylum":
                     name = name.replace("Candidatus ", "")
                 elif rank == "family":
                     name = name.replace("uncultured ", "")
-                submittable, taxid = ena.query_scientific_name(name)
+                submittable, taxid = self.query_scientific_name(name)
 
         return submittable, name, taxid
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Find taxID and scientific name from a lineage string"
+    )
+    parser.add_argument(
+        "-i", "--input",
+        required=True,
+        help="Input file with lineages one per line"
+    )
+    args = parser.parse_args()
+    with open(args.input, 'r') as file_in:
+        for line in file_in:
+            line = line.strip()
+            finder = TaxonFinder(line)
+            print(f"Lineage: {line}")
+            print(f"Scientific name: {finder.scientific_name}")
+            print(f"TaxID: {finder.taxid}")
+
+
+if __name__ == "__main__":
+    main()
