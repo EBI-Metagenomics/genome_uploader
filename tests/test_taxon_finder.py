d6d6e206d70bc92f09aabe9c038ed27c6d49d01e
Ekaterina Sakharova <kates@ebi.ac.uk>
2025-10-05 22:53:27 +0100

    fix

--- tests/test_taxon_finder.py
+++ tests/test_taxon_finder.py
@@ -4,15 +4,15 @@ from genomeuploader.taxon_finder import TaxonFinder
 def test_taxon_finder_bacteria():
     # unknown species
     finder = TaxonFinder('d__Bacteria;p__Pseudomonadota;c__Alphaproteobacteria;o__;f__;g__;s__')
-    assert finder.taxid == 1913988
+    assert finder.taxid == '1913988'
     assert finder.scientific_name == 'Alphaproteobacteria bacterium'
 
     # full lineage
     finder = TaxonFinder('d__Bacteria;p__Bacteroidota;c__Bacteroidia;o__Bacteroidales;f__Bacteroidaceae;g__Phocaeicola;s__Phocaeicola plebeius')
-    assert finder.taxid == 310297
+    assert finder.taxid == '310297'
     assert finder.scientific_name == 'Phocaeicola plebeius'
 
     # species with sp.
     finder = TaxonFinder('d__Bacteria;p__Bacillota;c__Clostridia;o__Eubacteriales;f__Oscillospiraceae;g__Ruminococcus;s__')
-    assert finder.taxid == 41978
+    assert finder.taxid == '41978'
     assert finder.scientific_name == 'Ruminococcus sp.'
